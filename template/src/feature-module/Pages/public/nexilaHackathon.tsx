a56cf03113982f8d2cc73b877002f862ed017306
Nexila <info@nexilatechnologies.com>
2026-09-24 18:15:38 +0530

    updating payment failure

--- template/src/feature-module/Pages/public/nexilaHackathon.tsx
+++ template/src/feature-module/Pages/public/nexilaHackathon.tsx
@@ -676,7 +676,7 @@ const NexilaHackathon: React.FC = () => {
             ================================================= */
 
             const orderResponse = await fetch(
-                "https://crm.nexilatechnologies.com:5000/api/hackathon/public/create-order",
+                "/api/hackathon/public/create-order",
                 {
                     method: "POST",
                     headers: {
@@ -789,7 +789,7 @@ const NexilaHackathon: React.FC = () => {
 
                             const verifyResponse =
                                 await fetch(
-                                    "https://crm.nexilatechnologies.com:5000/api/hackathon/public/verify-payment",
+                                    "/api/hackathon/public/verify-payment",
                                     {
                                         method: "POST",
 
@@ -904,7 +904,7 @@ const NexilaHackathon: React.FC = () => {
                 try {
 
                     await fetch(
-                        "https://crm.nexilatechnologies.com:5000/api/hackathon/public/payment-failed",
+                        "/api/hackathon/public/payment-failed",
                         {
                             method: "POST",
 
